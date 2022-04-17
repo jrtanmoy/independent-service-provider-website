844666852929bfbfe0bd92eb180ae8b0c0208218
jrtanmoy <roy28301@gmail.com>
2022-04-17 12:08:22 +0600

    create bunch of components & set Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
-import Banner from './components/Banner/Banner';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import CheckoutOrder from './components/Checkout/CheckoutOrder';
 import Footer from './components/Footer/Footer';
-import FrequentlyAsked from './components/FrequentlyAsked/FrequentlyAsked';
 import Header from './components/Header/Header';
-import Services from './components/Services/Services';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Banner></Banner>
-      <Services></Services>
-      <FrequentlyAsked></FrequentlyAsked>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/checkout" element={<CheckoutOrder></CheckoutOrder>
+        }></Route>
+        
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        
+      </Routes>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/Checkout/CheckoutOrder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckoutOrder = () => {
+    return (
+        <div>
+            <h1>This is checkout</h1>
+        </div>
+    );
+};
+
+export default CheckoutOrder;
