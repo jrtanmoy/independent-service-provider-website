4a58a00478f02c40212ddbff817c541a6468f807
jrtanmoy <roy28301@gmail.com>
2022-04-17 12:15:33 +0600

    create notfound page and connect with router

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
@@ -19,11 +20,10 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/checkout" element={<CheckoutOrder></CheckoutOrder>
         }></Route>
-        
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
-        
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div>
+            <section className='container text-center'>
+                <div className=''>
+                    <div>
+                        <h1>
+                            Oops! The page you're looking for isn't here.
+                        </h1>
+                        <p className=''>
+                            You might have the wrong address, or the page may have moved.
+                        </p>
+
+                    </div>
+                    <div>
+                        <div className='error-img'>
+                            <img
+                                src='https://thumbs.dreamstime.com/b/%C3%B0%C2%BF%C3%B0%C2%B5%C3%B1%E2%80%A1%C3%B0%C2%B0%C3%B1%E2%80%9A%C3%B1%C5%93-134036857.jpg'
+                                alt=''
+                            />
+                        </div>
+                        <span className='around-link'>
+                            <Link
+                                to='/'
+                                className='homepage-link'
+                            >
+                                Back to homepage
+                            </Link>
+                        </span>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default NotFound;
