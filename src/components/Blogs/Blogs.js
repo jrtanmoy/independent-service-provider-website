99bb14605cde47c851fb24c27b8fdc466eee1682
jrtanmoy <roy28301@gmail.com>
2022-05-30 18:27:44 +0600

    some update

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -10,6 +10,7 @@ const Blogs = () => {
                 <p className='fs-5 fst-italic'>b) Authentication works through passwords and other information provided by the user but Authorization works through settings that are implemented and maintained by the organization.</p>
                 <p className='fs-5 fst-italic'>c) Authentication is the first step of a good identityon the other hand Authorization always takes place after authentication.</p>
             </div>
+
             <div>
                 <h4>2. Why are you using firebase? What other options do you have to implement authentication?</h4>
                 <p className='fs-5 fst-italic'>Ans : Firebase Authentication provides us backend services, SDKs, and ready-made UI libraries to authenticate users to our app. It supports authentication using passwords, phone numbers, Google, Facebook and Twitter, and many more. It's also make error handling easy. Than's why we are using Firebase Authentication.</p>
@@ -28,6 +29,7 @@ const Blogs = () => {
                     </ol>
                 </p>
             </div>
+            
             <div>
                 <h4>3. What other services does firebase provide other than authentication?</h4>
                 <p className='fs-5 fst-italic'>Ans : </p>
